4620cf99460917bb872bb6139afefd8f7bfad27a
Eysenck Gandah <nyashafpl@gmail.com>
2025-03-11 21:51:04 +0200

    Updated homepage

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/my-portfolio-profile">
+      <BrowserRouter basename="/nyasha-gandah-portfolio">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
